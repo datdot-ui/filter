0714817b7a63d89ae20df5140b8ee56474a2a014
Fiona Ye <fiona.ye@yandex.com>
2020-12-17 23:20:51 +0800

    update

---
+++ demo/demo.js
@@ -0,0 +1,141 @@
+const bel = require('bel')
+const csjs = require('csjs-inject')
+const path = require('path')
+const filename = path.basename(__filename)
+const option = require('..')
+const domlog = require('ui-domlog')
+
+function demoComponent() {
+    let recipients = []
+    const data = ['Available', 'Not available', 'Hypercore', 'Hyperdrive', 'Cabal']
+
+    // content
+    const content = bel`
+    <div class=${css.content}>
+        ${option({page: 'demo', flow: 'filter', name: 'filter-option', data}, protocol('filter-option'))}
+    </div>`
+    
+    // show logs
+    let terminal = bel`<div class=${css.terminal}></div>`
+    // container
+    const container = wrap(content, terminal)
+    return container
+
+    function wrap (content) {
+        const container = bel`
+        <div class=${css.wrap}>
+            <section class=${css.container}>
+                ${content}
+            </section>
+            ${terminal}
+        </div>
+        `
+        return container
+    }
+
+    function activeOption (message) {
+        const { page, from, flow } = message
+        let state = recipients[from].state
+        if (state === undefined) recipients[from].state = 'self-active'
+        if (state === 'self-active') recipients[from].state = 'remove-active'
+        if (state === 'remove-active') recipients[from].state = 'self-active'
+        recipients[from]({page, from, flow, type: recipients[from].state, filename, line: 42})
+    }
+
+    // original protocol for all use
+    function protocol (name) {
+        return send => {
+            recipients[name] = send
+            return receive
+        }
+    }
+
+    // receive
+    function receive (message) {
+        const { page, from, flow, type, action, body } = message
+        showLog(message)
+        if (type === 'init') showLog({page: 'demo', from, flow, type: 'ready', body, filename, line: 57})
+        if (type === 'click') { 
+           if (from === 'filter-option') activeOption(message)
+            data.forEach( name => {
+                if (from === name) showLog({page: 'demo', from, flow, type: 'unchecked', filename, line: 61})
+            }) 
+        }
+    }
+
+    // keep the scroll on bottom when the log displayed on the terminal
+    function showLog (message) { 
+        sendMessage(message)
+        .then( log => {
+            terminal.append(log)
+            terminal.scrollTop = terminal.scrollHeight
+        }
+    )}
+
+
+    /*********************************
+    * ------ Promise() Element -------
+    *********************************/
+    async function sendMessage (message) {
+        return await new Promise( (resolve, reject) => {
+            if (message === undefined) reject('no message import')
+            const log = domlog(message)
+            return resolve(log)
+        }).catch( err => { throw new Error(err) } )
+    }
+    
+}
+
+const css = csjs`
+*, *:before, *:after {
+    box-sizing: inherit;
+}
+html {
+    box-sizing: border-box;
+    height: 100%;
+}
+body {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 14px;
+    background-color: rgba(0, 0, 0, .1);
+    height: 100%;
+}
+.wrap {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 75vh 25vh;
+}
+.container {
+    padding: 25px;
+    overflow-y: auto;
+}
+.container > div {
+    margin-bottom: 20px;
+}
+.content > div button {
+    margin-right: 10px;
+}
+.terminal {
+    background-color: #212121;
+    color: #f2f2f2;
+    font-size: 13px;
+    overflow-y: auto;
+}
+.hide {
+    animation: disppear .5s linear forwards;
+}
+@keyframes disppear {
+    0% {
+        opacity: 1;
+        top: 53px;
+    }
+    100% {
+        opacity: 0;
+        top: 45px;
+    }
+}
+`
+
+document.body.append( demoComponent() )
---
+++ src/index.js
@@ -0,0 +1,244 @@
+const bel = require('bel')
+const csjs = require('csjs-inject')
+const button = require('datdot-ui-button')
+const svg = require('datdot-ui-graphic')
+const path = require('path')
+const filename = path.basename(__filename)
+
+module.exports = filterOption
+
+function filterOption ({page, flow, name, data}, protocol) {
+    const widget = 'ui-filter-option'
+    const send2Parent = protocol( receive )
+    send2Parent({page, from: name, flow: flow ? `${flow}/${widget}` : widget, type: 'init', filename, line: 13})
+    let recipients = []
+    // icon
+    const iconOption = svg( { css: `${css.icon} ${css['icon-option']}`, path: 'assets/option.svg' })
+    // button
+    const filterOption = button({page, flow: flow ? `${flow}/${widget}` : widget,  name: 'filter-option', content: iconOption, style: 'default', color: 'fill-grey'}, optionProtocol('filter-option'))
+    const optionAction = bel`<div class="${css.action} ${css.option}">${filterOption}</div>`
+    // filter option
+    const optionList = bel`<ul class="${css['option-list']}" onclick=${(e) => actionOptionList(e)}></ul>`
+   
+    // get lits
+    optionListRender(data).then( buttons => {
+        buttons.map( item => { 
+            const li = bel`<li>${item}</li>`
+            optionList.append(li) 
+        })
+    })
+
+    return optionAction
+
+    /*************************
+    * ------- Actions --------
+    *************************/
+    function actionOptionList (event) {
+        const target = event.target
+        if (!target.classList.contains(css.status)) return
+        const icon = target.firstChild
+        // for recipients[name] using
+        const name = `option-${target.innerText.toLowerCase().split(' ').join('')}`
+        // if icon is not contained css.hide then do toggling it on unchecked/checked 
+        const type = icon.classList.contains(css.checked) ? 'unchecked' : 'checked'
+
+        if (type === 'checked') { 
+            icon.classList.add(css.checked)
+            icon.classList.remove(css.unchecked)
+        }
+
+        if (type === 'unchecked') {
+            icon.classList.remove(css.checked)
+            icon.classList.add(css.unchecked)
+        }
+        
+        recipients[name]({page: 'demo', from: target.innerText, flow: 'option-list', type, body: name})
+        const message = {page: 'demo', from: target.innerText, flow: 'option-list', type, body: name, filename, line: 156}
+        send2Parent(message)
+    }
+
+    function displayOptionList (message) {
+        const {page, from, flow, type, body, action} = message
+        let log = {page, from, flow, type: 'active', body, filename, line: 52}
+        recipients[from](log)
+        optionAction.append(optionList)
+        if (optionList.children.length > 0) optionList.classList.remove(css.hide)
+        return send2Parent(log)
+    }
+
+    function hideOptionList (message) {
+        const {page, from, flow, type, body, action} = message
+        let log = {page, from, flow, type, body, filename, line: 61}
+        recipients[from](log)
+        optionList.classList.add(css.hide)
+        return send2Parent(log)
+    }
+
+    function actionFilterOption (message) {
+        const { type } = message
+        if (type === 'self-active') displayOptionList(message)
+        if (type === 'remove-active') hideOptionList(message)
+    }
+
+    /*************************
+    * ------- Protocol --------
+    *************************/
+    function optionProtocol (name) {
+        return send => {
+            recipients[name] = send
+            return receive
+        }
+    }
+
+    /*************************
+    * ------ Receivers -------
+    *************************/
+    function receive (message) {
+        const {page, flow, from, type, action, body} = message
+        if ( from === 'filter-option') actionFilterOption(message)
+        return send2Parent(message)
+    }
+
+    /*********************************
+    * ------ Promise() Element -------
+    *********************************/
+    async function optionListRender (data) {
+        return await new Promise((resolve, reject) => {
+            if (data === undefined) reject( )
+            const lists = data.map( item => {
+                let style
+                const check = svg( { css: `${css.icon} ${css['icon-check']} ${css.checked}`, path: 'assets/check.svg' })
+                if (item === 'Available') style = css.online
+                if (item === 'Not available') style = css.offline
+                if (item === 'Hypercore') style = css.core
+                if (item === 'Hyperdrive') style = css.drive
+                if (item === 'Cabal') style = css.cabal
+                const content = bel`<div class=${css.status}>${check}<span class="${css.circle} ${style}"></span>${item}</div>`
+                const btn = button({page, flow: flow ? `${flow}/${widget}` : widget, name: item, content, style: 'link', color: 'link-white'}, optionProtocol(`option-${item.toLowerCase().split(' ').join('')}`))
+                return btn
+            })
+            return resolve(lists)
+        }).catch( err => { throw new Error(err)} )
+    }
+
+}
+
+const css = csjs`
+.option {
+    position: relative;
+    display: grid;
+    justify-items: right;
+    width: 150px;
+}
+.option > button[class^="btn"] {
+    position: relative;
+    z-index: 3;
+    margin-right: 0;
+}
+.option-list {
+    position: absolute;
+    z-index: 2;
+    right: 0;
+    width: 100%;
+    animation: showup .25s linear forwards;
+}
+.option-list, .option-list li  { 
+    margin: 0; 
+    padding: 0;
+    list-style: none;
+}
+.option-list li > button {
+    background-color: #000;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    text-align: left;
+    transition: background-color 0.3s linear;
+}
+.option-list li:first-child > button {
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+}
+.option-list li:last-child > button {
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+}
+.option-list li > button:hover {
+    color: #fff;
+    background-color: #666;
+}
+.option-list .icon-check {
+    pointer-events: none;
+}
+.option-list .icon-check svg path {
+    stroke: #fff;
+}
+.status {
+    display: grid;
+    grid-template-rows: 32px;
+    grid-template-columns: 18px 27px auto;
+    padding: 0 10px;
+    align-items: center;
+}
+.circle {
+    display: block;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    background-color: #000;
+    justify-self: center;
+    pointer-events: none;
+}
+.online {
+    background-color: #109B36;
+}
+.offline {
+    background-color: #D9D9D9;
+}
+.core {
+    background-color: #BCE0FD;
+}
+.drive {
+    background-color: #FFDFA2;
+}
+.cabal {
+    background-color: #E9D3FD;
+}
+.icon {
+    width: 16px;
+}
+.icon-check {
+    opacity: 0;
+    transition: opacity .25s linear;
+}
+.icon-option {}
+.hide {
+    animation: disappear .25s linear forwards;
+}
+.checked {
+    opacity: 1;
+}
+.unchecked {
+    opacity: 0;
+}
+@keyframes showup {
+    0% {
+        opacity: 0;
+        top: 45px;
+    }
+    100% {
+        opacity: 1;
+        top: 53px;
+    }
+}
+@keyframes disappear {
+    0% {
+        opacity: 1;
+        top: 53px;
+    }
+    100% {
+        opacity: 0;
+        top: 45px;
+    }
+}
+`
